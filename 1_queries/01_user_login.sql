1126452c5d78464340f58dea57e8aff7fff752dc
Winnie Li <liwanyi0313@gmail.com>
2022-08-25 22:54:17 +0000

    refactor getUserWithEmail to retrieve data from database instead of the in-memory data

--- 1_queries/01_user_login.sql
+++ 1_queries/01_user_login.sql
@@ -1,5 +1,4 @@
 -- When a user logs in, we will need to get all of their details to compare their email and password, and to show them a customized home page.
-
 SELECT id, name, email, password
 FROM users
 WHERE email = 'tristanjacobs@gmail.com';
